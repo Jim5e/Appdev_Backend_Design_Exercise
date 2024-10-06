e712e46b69bcc815bbcbe9b0f75703fef76ce481
Your Name <jamesterenceclarit@gmail.com>
2024-10-06 15:35:18 +0800

    made routes more consistent

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -7,7 +7,7 @@ const rateLimiter = require('../middleware/rateLimiter');
 // define the routes
 router.post('/Login', rateLimiter, userController.loginUser);
 router.post('/Register', rateLimiter, userController.registerUser);
-router.get('/userProfile', rateLimiter, authenticateToken, userController.getUserProfile);
-router.get('/getAllUsers', rateLimiter, userController.getAllUsers);
+router.get('/UserProfile', rateLimiter, authenticateToken, userController.getUserProfile);
+router.get('/AllUsers', rateLimiter, userController.getAllUsers);
 
 module.exports = router;
